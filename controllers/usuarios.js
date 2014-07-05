df5f7bae448b3dea74df5994082d39d27c43f639
rafael <rreynoud@gmail.com>
2014-07-05 14:38:19 -0300

    Aula 16 Eliminar Usuario Funcionando

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -86,7 +86,7 @@ module.exports = function(app){
 				if (err){
 					console.log(err);
 				}else{
-					req.flash('info', 'Usuário excluído com sucesso!');
+					//req.flash('info', 'Usuário excluído com sucesso!');
 					res.redirect('/usuarios');
 				}
 			});
